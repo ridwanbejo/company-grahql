b155eb5b27caf46c49dce3162593053017b7dd64
Ridwan Fadjar <ridwan@sense-os.nl>
2018-01-31 10:11:02 +0700

    add basic auth using jwt, bodyparser for graphql and json

--- app.js
+++ app.js
@@ -3,16 +3,43 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var jwt = require("jsonwebtoken"); 
 var exJwt = require("express-jwt");
+var bodyParser = require("body-parser");
 
 const schema = require("./src/schemas.js");
 
 var app = express();
+var router = express.Router();
 
-app.post('/auth-token', function(req, res){
+var secretToken = "secret12345";
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.text({ type: 'application/graphql' }));
+
+router.get('/hello', function(req, res){
+	res.send("Hello World!");
 });
 
-app.use('/graphql', exJwt({secret: "secret12345"}), graphqlHTTP({
+router.post('/auth-token', function(req, res){
+	console.log(req.body);
+	if (req.body.username == "admin" && req.body.password == "secret12345")
+	{
+		var token = jwt.sign({
+			username: req.body.username,
+			role: ["admin"]
+		}, secretToken, {});
+		res.json({token: token});
+	}
+	else
+	{
+		res.json({msg: "Unknown Account!"});
+	}
+	
+});
+
+app.use ("/", router);
+
+app.use('/graphql', exJwt({secret: secretToken}), graphqlHTTP({
 	schema: schema,
 	graphiql: true
 }));
